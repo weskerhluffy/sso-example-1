85ddd5212247eccd4364ea0f9a8cd29e80b3c77c
Darkly Dreaming Wesker <weskerhluffy@gmail.com>
2014-06-29 18:18:11 -0500

    añadido dump de bd de caCAS

--- grails-app/conf/spring/resources.groovy
+++ grails-app/conf/spring/resources.groovy
@@ -3,9 +3,6 @@
 
 beans = {
 	userDetailsFromDomainClassFactory(mx.mierda.ass.CacaUserDetailsFromDomainClassFactory)
-	//	authenticationUserDetailsService(org.codehaus.groovy.grails.plugins.springsecurity.cas.CasAuthenticationUserDetailsService)
-	//	domainUserMapperService(org.codehaus.groovy.grails.plugins.springsecurity.cas.DomainUserMapperService)
-	//	userMapper(org.codehaus.groovy.grails.plugins.springsecurity.cas.DomainUserMapperService)
 	registrar(codeisdesign.example.sso.mierda.CustomPropertyEditorRegistrar)
 	configurer(org.springframework.beans.factory.config.CustomEditorConfigurer) {
 		propertyEditorRegistrars = [ref("registrar")]
